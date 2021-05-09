dea712135ebcc0aff34443c2f3450c8dbbfeab68
joeputra <joe.putra40@gmail.com>
2021-05-09 14:49:31 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Movie App w/ React
+
+
+## API Stuff
+featured: "https://api.themoviedb.org/3/discover/movie?
+sort_by=popularity.desc&
+api_key=04c35731a5ee918f014970082a0088b1&page=1"
+Images : "https://image.tmdb.org/t/p/w1280"
+Search: "https://api.themoviedb.org/3/serach/movie?&
+api_key=04c35731a5ee918f014970082a0088b1&
+query="
+
+
+## Youtube Tutorial
+https://www.youtube.com/watch?v=sZ0bZGfg_m4&list=PLMFi-EYN-ONo6hE-fbPA5OgctbFpmj-Ol&index=46&t=19s
+
+## Min
+10:13
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import Movie from './component/Movie'
+import './App.css';
+
+
+
+const FEATURE_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = "https://api.themoviedb.org/3/serach/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+function App() {
+  const [ movies, setMovies ] = useState ([]);
+
+  useEffect(()=> {
+    fetch(FEATURE_API)
+    .then((res)=> res.json())
+    .then((data)=> {
+      console.log(data)
+      setMovies(data);
+    })
+
+  }, [])
+
+  return (
+    <div>
+      {movies.length> 0 && movies.map((movie)=>
+        <Movie />
+      )}
+    </div>
+  );
+}
+
+export default App;
